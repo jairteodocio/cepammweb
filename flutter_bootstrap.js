2c50105027dc7e95b0ffe95bd603c782dd56642d
jairteodocio <jairerickiker@gmail.com>
2025-03-20 21:10:30 -0600

    actualizacion2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "348647442"
+    serviceWorkerVersion: "1345199716"
   }
 });
